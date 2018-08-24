e4619658eeafdf6474b07be86dc6a42814928587
QuentinDll <quentin.delplace13@gmail.com>
2018-08-24 09:39:03 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+// creation de la fonction yes
+function yes(){
+  return true;
+}
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 1</title>
+</head>
+<body>
+  <p>
+    <!-- affichage de la fonction -->
+    <?= echo yes();?>
+  </p>
+</body>
+</html>
